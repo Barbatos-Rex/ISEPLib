7c68fdaad740bf12a71b81174bb0422b9d7568f4
Barbatos_Rex <1190387@isep.ipp.pt>
2020-01-13 00:27:39 +0000

    Implemented constructor.

--- src/barbatos_rex/PyVariable.java
+++ src/barbatos_rex/PyVariable.java
@@ -1,11 +1,46 @@
 package barbatos_rex;
+import java.lang.NumberFormatException;
 
 public class PyVariable {
-    private String stringValue = "";
+    private String stringValue = null;
     private int intValue =0;
     private double doubleValue=0.0;
-    private long longValue=0;
-    private 
+    private long longValue= 0;
+    private String myType= null;
+
+
+    public PyVariable(String value, String type){
+        type = type.toLowerCase();
+        switch(type){
+            case "int":
+                if(Syntax.isInt(value)){
+                    this.intValue=Integer.parseInt(value);
+                }else{
+                    throw new NumberFormatException("The String cannot be converted to int");
+                }
+                break;
+            case "double":
+                if(Syntax.isAParsableNumber(value)){
+                    this.doubleValue=Double.parseDouble(value);
+                }else {
+                    throw new NumberFormatException("The String cannot be converted to double");
+                }
+                break;
+            case "long":
+                if(Syntax.isInt(value)){
+                    this.longValue=Long.parseLong(value);
+                }else{
+                    throw new NumberFormatException("The String cannot be converted to long");
+                }
+                break;
+            case "string":
+                    this.stringValue=value;
+                break;
+            default:
+                throw new ClassFormatError("Chosen type is invalid (Either not implemented or not valid, realy wtf!)");
+        }
+
+    }
 
 
 
--- src/barbatos_rex/Syntax.java
+++ src/barbatos_rex/Syntax.java
@@ -10,6 +10,17 @@ public class Syntax {
         }
         return -1;
     }
+    public static boolean isInt(String num){
+        boolean flag1=isAParsableNumber(num);
+        boolean flag2=true;
+        for(int i=0;i<num.length();i++){
+            if (num.charAt(i) == ',') {
+                flag2 = false;
+                break;
+            }
+        }
+        return flag1 && flag2;
+    }
     public static boolean isAParsableNumber(String str){
         try {
             Double.parseDouble(str);
